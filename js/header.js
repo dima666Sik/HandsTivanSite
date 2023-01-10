62391cc06fd8f9112604dd1622362bd78d927558
Lazyness <leniviy.demon.1@gmail.com>
2023-01-10 22:02:48 +0200

    Create header, with style and adaptive to mobile. Create one use many times. We must to add drop-down list to word 'Послуги'.

---
+++ js/header.js
@@ -0,0 +1,28 @@
+document.write('<header id="header" class="header">\
+<div class="header-container">\
+  <div class="container">\
+    <div class="menu">\
+      <!-- Logo -->\
+      <a href="index.html"><img src="../img/logo_massage.png" alt="logo" width="7%" /></a>\
+      <!-- Hamburger icon -->\
+      <input class="side-menu" type="checkbox" id="side-menu" />\
+      <label class="hamb" for="side-menu"\
+        ><span class="hamb-line"></span\
+      ></label>\
+      <!-- Menu -->\
+      <nav class="nav">\
+        <ul class="menu">\
+          <li><a class="services-ul" href="#services">Послуги</a></li>\
+          <li><a href="#rules">Правила</a></li>\
+          <li><a href="#contacts">Контакти</a></li>\
+          <li><a href="#price-and-discounts">Ціни та знижки</a></li>\
+          <li><a href="#trainings">Тренінги</a></li>\
+          <li>\
+            <a href="#gift-certificates">Подарункові сертифікати</a>\
+          </li>\
+        </ul>\
+      </nav>\
+    </div>\
+  </div>\
+</div>\
+</header>')
