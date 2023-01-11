0fd6b913462ed01f0b50e2ce92f163af23e11eb8
Lazyness <leniviy.demon.1@gmail.com>
2023-01-11 17:35:13 +0200

    Update header. Our actions: shange style, add change language.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Tivan massage site 🙌
 
+## Preview from dima-coder =)!
+1. This project use html 5, css, js.
+2. Project is future site.
+
+## Demo
+You can check my work by clicking on this link: 
+
+## Features
+- Adaptive layout
+
+### Header
+- Drop-down list
+- Change language (Ukrainian|English)
+
+### Footer
+
--- js/header.js
+++ js/header.js
@@ -1,39 +1,49 @@
-document.write('<header id="header" class="header">\
-<div class="header-container">\
-  <div class="container">\
-    <div class="menu">\
+document.write('    <header id="header" class="header">\
+<div class="container">\
+  <div class="container-fl">\
+    <div class="logo-container">\
       <!-- Logo -->\
-      <img src="../img/logo_massage.png" alt="logo" width="7%" />\
-      <!-- Hamburger icon -->\
-      <input class="side-menu" type="checkbox" id="side-menu" />\
-      <label class="hamb" for="side-menu"\
-        ><span class="hamb-line"></span\
-      ></label>\
-      <!-- Menu -->\
-      <nav class="nav">\
-        <ul class="menu">\
-          <li>\
-            <a class="services-ul" href="#services">Послуги</a>\
-            <ul class="menu-under">\
-              <li><a href="#">SPA програми</a></li>\
-              <li><a href="#">Тайський масаж</a></li>\
-              <li><a href="#">Гавайський масаж</a></li>\
-              <li><a href="#">Вогняний масаж</a></li>\
-              <li><a href="#">Стоун-терапія</a></li>\
-              <li><a href="#">Антицелюлітний масаж</a></li>\
-              <li><a href="#">Масаж обличчя</a></li>\
-              <li><a href="#">Для двох</a></li>\
-            </ul>\
-          </li>\
-          <li><a href="#rules">Правила</a></li>\
-          <li><a href="#contacts">Контакти</a></li>\
-          <li><a href="#price-and-discounts">Ціни та знижки</a></li>\
-          <li><a href="#trainings">Тренінги</a></li>\
-          <li>\
-            <a href="#gift-certificates">Подарункові сертифікати</a>\
-          </li>\
-        </ul>\
-      </nav>\
+      <img class="logo" src="../img/logo_massage.png" alt="logo" />\
+    </div>\
+    <div class="menu-container">\
+      <div class="div-menu">\
+        <!-- Hamburger icon -->\
+        <input class="side-menu" type="checkbox" id="side-menu" />\
+        <label class="hamb" for="side-menu"\
+          ><span class="hamb-line"></span\
+        ></label>\
+        <!-- Menu -->\
+        <nav class="nav">\
+          <ul class="menu">\
+            <li>\
+              <a class="services-ul" href="#services">Послуги</a>\
+              <ul class="menu-under">\
+                <li><a href="#">SPA програми</a></li>\
+                <li><a href="#">Тайський масаж</a></li>\
+                <li><a href="#">Гавайський масаж</a></li>\
+                <li><a href="#">Вогняний масаж</a></li>\
+                <li><a href="#">Стоун-терапія</a></li>\
+                <li><a href="#">Антицелюлітний масаж</a></li>\
+                <li><a href="#">Масаж обличчя</a></li>\
+                <li><a href="#">Для двох</a></li>\
+              </ul>\
+            </li>\
+            <li><a href="#rules">Правила</a></li>\
+            <li><a href="#contacts">Контакти</a></li>\
+            <li><a href="#price-and-discounts">Ціни та знижки</a></li>\
+            <li><a href="#trainings">Тренінги</a></li>\
+            <li>\
+              <a href="#gift-certificates">Подарункові сертифікати</a>\
+            </li>\
+          </ul>\
+        </nav>\
+      </div>\
+      <div class="select">\
+        <select name="slct" id="slct">\
+          <option value="ua" selected>UA</option>\
+          <option value="en">EN</option>\
+        </select>\
+      </div>\
     </div>\
   </div>\
 </div>\
