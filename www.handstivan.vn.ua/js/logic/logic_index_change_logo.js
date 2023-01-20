47d376bbfb6e2164eb914b7058dc94d333891060
Lazyness <leniviy.demon.1@gmail.com>
2023-01-20 22:10:53 +0200

    fix header

--- www.handstivan.vn.ua/js/logic/logic_index_change_logo.js
+++ www.handstivan.vn.ua/js/logic/logic_index_change_logo.js
@@ -1,9 +1,18 @@
 const getLogo = document.querySelector(".logo__container");
+const getSelectLang = document.querySelector(".select");
 const getHamb = document.querySelector(".hamb");
 getHamb.addEventListener("click", changeSizeLogo);
 let flagClick = true;
 
 function changeSizeLogo() {
-    if (flagClick) { getLogo.style.maxWidth = "50%"; flagClick = false; }
-    else { getLogo.style.maxWidth = ""; flagClick = true; }
+    if (flagClick) { 
+        getLogo.style.maxWidth = "50%";
+        getSelectLang.style.display = 'none';
+        flagClick = false; 
+    }
+    else { 
+        getLogo.style.maxWidth = "";
+        getSelectLang.style.display = "";
+        flagClick = true; 
+    }
 }
